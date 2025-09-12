c59a72fdc6c7f0266b28cb1420efc2b12c35538b
Tomáš Peťovský <tompetko@gmail.com>
2025-09-12 16:27:33 +0200

    Cleanup.

--- NineDigit.SerialTransport/Platforms/Android/IUsbPermissionService.cs
+++
@@ -1,8 +0,0 @@
-// #if ANDROID
-// namespace NineDigit.SerialTransport;
-//
-// public interface IUsbPermissionService
-// {
-//     Task<bool> EnsureDevicePermissionAsync(int vendorId, int productId, CancellationToken cancellationToken = default);
-// }
-// #endif
